6864d97915e9493920e903c42cb88bf917ecad91
lancao2 <lancao.af@gmail.com>
2022-09-15 22:14:06 -0300

    feat: implement authorization token

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,33 +1,37 @@
 const mongoose = require("mongoose");
-const  UserRegister = require("../models/RegisterModel");
-const bcrypt = require('bcryptjs')
+const UserRegister = require("../models/RegisterModel");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+require("dotenv/config");
 
+const loginModel = mongoose.model("User", UserRegister.UserSchema);
 
-
-const loginModel = mongoose.model("User", UserRegister.UserSchema)
-
-
-  class login extends UserRegister.register {
-       constructor(body){
-        super(body)
-       }
-   
-    async toLogin(){
-        this.validate()
-        if(this.errors.length > 0 ) return
-        this.user = await loginModel.findOne({mail: this.body.mail})
-
-        if(!this.user) {
-            this.errors.push('username or password is invalid')
-            return
-        }
-
-        if(!bcrypt.compareSync(this.body.password, this.user.password)){
-            this.errors.push('username or password is invalid')
-            return
-        }
-    }
-
+class login extends UserRegister.register {
+  constructor(body) {
+    super(body);
+    this.token = ""
   }
 
- module.exports = login 
+  async toLogin(mail, password) {
+    this.validate();
+    if (this.errors.length > 0) return;
+    this.user = await loginModel.findOne({mail});
+
+    if (!this.user) {
+      this.errors.push("Email or password is invalid");
+      
+    }
+    if (!bcrypt.compareSync(password, this.user.password)) {
+      this.errors.push("Email or password is invalid");
+      return;
+    }
+    this.user.password = undefined;
+
+    this.token = jwt.sign({_id: this.user._id}, process.env.SECRET_TOKEN, {
+       expiresIn:  process.env.EXPIRATION_TOKEN
+    })
+  }
+
+}
+
+module.exports = login;
