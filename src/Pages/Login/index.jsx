3430a74923001cbc0384bf2f1577911f51e3fcf6
lancao2 <lancao.af@gmail.com>
2022-09-26 10:33:07 -0300

    feat: forms validation

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -2,29 +2,88 @@ import Emojes from "../../Components/Emojes";
 import { DivHome } from "../home/styles";
 import { DivForm, Button, Input } from "./styles";
 import { useHistory } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faLock, } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
-const LoginPage = () =>{
+const Loginschema = yup
+  .object()
+  .shape({
+    email: yup
+      .string()
+      .required("email is required")
+      .matches(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      ),
+    password: yup.string().required("Password is required").min(6),
+  })
+  .required();
 
-    let history = useHistory();
-    function handleClick() {
+const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(Loginschema),
+  });
+
+  let history = useHistory();
+  function handleClick() {
     history.push("/register");
-    }
+  }
 
-    return(
-        <DivHome>
-            <Emojes/>
-            <DivForm action="submit">
-                <h1>Login</h1>
-                <Input><FontAwesomeIcon color="#0494E5" icon={faEnvelope}/><input type="text" placeholder="E-mail" /></Input>
-                <Input><FontAwesomeIcon color="#0494E5" icon={faLock}/><input type="password" placeholder="Password"/></Input>
-                <div className="buttons"><Button>Entrar</Button></div>
-                <p>Não tem conta?<a className="singin" onClick={handleClick}>Cadastre-se!</a></p>
-                <span>Este site não pede informações pessoais nem de pagamento </span>
-            </DivForm>
-        </DivHome>
-    )
-}
+  return (
+    <DivHome>
+      <Emojes />
+      <DivForm onSubmit={handleSubmit(e=> console.log(e))}>
+        <h1>Login</h1>
+        {!errors.email ? (
+          <Input>
+            <FontAwesomeIcon color="#0494E5" icon={faEnvelope} />
+            <input {...register("email")} type="text" placeholder="E-mail" />
+          </Input>
+        ) : (
+          <Input error={true}>
+            <FontAwesomeIcon color="red" icon={faEnvelope} />
+            <input
+              {...register("email")}
+              type="text"
+              placeholder={errors.email?.message}
+            />
+          </Input>
+        )}
+        {!errors.password ? <Input>
+          <FontAwesomeIcon color="#0494E5" icon={faLock} />
+          <input
+            {...register("password")}
+            type="password"
+            placeholder="Password"
+          />
+        </Input>: <Input error={true}>
+          <FontAwesomeIcon color="red" icon={faLock} />
+          <input
+            {...register("password")}
+            type="password"
+            placeholder={errors.password?.message}
+          />
+        </Input>}
+        
+        <div className="buttons">
+          <Button>Entrar</Button>
+        </div>
+        <p>
+          Não tem conta?
+          <a className="singin" onClick={handleClick}>
+            Cadastre-se!
+          </a>
+        </p>
+        <span>Este site não pede informações pessoais nem de pagamento </span>
+      </DivForm>
+    </DivHome>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
--- src/Pages/Register/index.jsx
+++ src/Pages/Register/index.jsx
@@ -4,22 +4,73 @@ import { DivForm, Input, Button } from "../Login/styles";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faLock, faUser, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import { useHistory } from "react-router-dom";
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import axios from "axios";
+
+
+
+const Registerschema = yup.object().shape({
+    username: yup.string().required("Username is required"),
+    email: yup.string().required("email is required").matches( /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/),
+    password: yup.string().required("Password is required").min(6),
+    confirmPassword: yup.string().required("Please confirm your password").oneOf([yup.ref("password")], "Password must match"),
+  })
+  .required();
+
+ 
 
 const RegisterPage = () => {
+
+
+  const newUser = data => registerUser(data)
+
+  const registerUser = (data) => {
+    
+    axios.post(`UrlBase/register`,{
+      username : data.username,
+      mail: data.email,
+      password: data.password,
+      headers: "application/json"
+    }).then(console.log(data)).catch(err => console.log(err))
+  
+  } 
+
+   
+  const { register, handleSubmit, formState: {errors} } = useForm({
+    resolver: yupResolver(Registerschema),
+  });
+
 
     let history = useHistory();
     function handleClick() {
     history.push("/login");
     }
 
+   
+
     return(
         <DivHome>
-            <DivForm>
-                <Input><FontAwesomeIcon color="#0494E5" icon={faUser}/><input type="text" placeholder="Usuario"/></Input>
-                <Input><FontAwesomeIcon color="#0494E5" icon={faEnvelope}/><input type="text" placeholder="Email"/></Input>
-                <Input><FontAwesomeIcon color="#0494E5" icon={faLock}/><input type="text" placeholder="Password"/></Input>
-                <Input><FontAwesomeIcon color="#0494E5" icon={faLock}/><input type="text" placeholder="Comfirm Password"/></Input>
-                <div className="buttons">
+            <DivForm onSubmit={handleSubmit(newUser)}>
+                <h1>Registro</h1>
+                {
+                !errors.username ?
+                 <Input><FontAwesomeIcon color="#0494E5" icon={faUser}/><input {...register("username")} type="text" placeholder="Usuario"/></Input> : 
+                 <Input error={true}><FontAwesomeIcon color="red" icon={faUser}/><input {...register("username")} type="text" placeholder={errors.username?.message}/></Input>}
+                {
+                !errors.email ? 
+                <Input><FontAwesomeIcon color="#0494E5" icon={faEnvelope}/><input {...register("email")}type="text" placeholder="Email"/></Input> : 
+                <Input error={true}><FontAwesomeIcon color="red" icon={faEnvelope}/><input {...register("email")}type="text" placeholder={errors.email?.message}/></Input>
+                }
+                {
+                !errors.password ? <Input><FontAwesomeIcon color="#0494E5" icon={faLock}/><input {...register("password")} type="password" placeholder="Password"/></Input> :
+                <Input error={true}><FontAwesomeIcon color="red" icon={faLock}/><input {...register("password")} type="password" placeholder={errors.password?.message}/></Input>
+                }
+                {
+                !errors.confirmPassword ?    
+                    <Input><FontAwesomeIcon color="#0494E5" icon={faLock}/><input type="password" {...register("confirmPassword")} placeholder="Comfirm Password"/></Input> : <Input error={true}><FontAwesomeIcon color="red" icon={faLock}/><input type="password" {...register("confirmPassword")} placeholder={errors.confirmPassword?.message}/></Input>}
+                <div className="buttons"> 
                 <Button>Registrar-se</Button>
                 <Button className="back" onClick={handleClick}><FontAwesomeIcon color="#ffffff" icon={faArrowLeft}/></Button>
                 </div>
