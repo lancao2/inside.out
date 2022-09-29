27ee904369a75320a1643fd1c99c144543fcb50f
lancao2 <lancao.af@gmail.com>
2022-09-28 21:55:40 -0300

    :bug: fix: show all friend request

--- src/controller/showFriendesRequestController.ts
+++ src/controller/showFriendesRequestController.ts
@@ -3,7 +3,7 @@ import { FriendModel } from "../models/friendModel";
 
 export const showFriendsRequest = async (req: Request, res: Response) => {
     try {
-      const friendesRequest = await FriendModel.findOne({ user: req.userId, isFriend: false});
+      const friendesRequest = await FriendModel.find({ user: req.userId, isFriend: false});
     res.status(201).json({ friendesRequest: friendesRequest });
   } catch (error) {
     res.status(400).json({ message: "algo deu errado" });
